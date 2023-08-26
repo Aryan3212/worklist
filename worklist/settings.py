e232c651dac1a6b73fbe898731615fbc6abc25b4
Aryan Rahman <rahman.aryan07@gmail.com>
2023-08-27 03:13:05 +0600

    removed prints

--- worklist/settings.py
+++ worklist/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-print(SECRET_KEY)
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-print(os.environ)
+
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
