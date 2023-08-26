0fd5cddf2c953561ba522b04a20a0bd3686cd482
Aryan Rahman <rahman.aryan07@gmail.com>
2023-08-27 02:55:58 +0600

    add static files

--- worklist/urls.py
+++ worklist/urls.py
@@ -2,6 +2,8 @@ from django.urls import include, path
 from rest_framework import routers
 from jobs import views
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -14,4 +16,4 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api/', include('rest_framework.urls', namespace='rest_framework'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
