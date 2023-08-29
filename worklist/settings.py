d187a7a21e0f3e820f04196f7e54e381b6874573
Aryan Rahman <rahman.aryan07@gmail.com>
2023-08-30 01:42:45 +0600

    fix

--- worklist/settings.py
+++ worklist/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://54.164.18.233/']
+ALLOWED_HOSTS = ['54.164.18.233']
 
 
 # Application definition
