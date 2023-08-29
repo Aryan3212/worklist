6257afd297ba1970f18a6e5ac6f2dcb78139e35c
Aryan Rahman <rahman.aryan07@gmail.com>
2023-08-30 02:58:34 +0600

    update cors

--- worklist/settings.py
+++ worklist/settings.py
@@ -62,7 +62,8 @@ CORS_ALLOWED_ORIGINS = [
     "https://localhost:3000",
     "http://127.0.0.1:3000",
     "https://127.0.0.1:3000",
-    '.worklist.club'
+    'worklist.club',
+    'www.worklist.club'
 ]
 
 ROOT_URLCONF = 'worklist.urls'
