c616786b0a3836a318ed0facef160fded4b07d71
Aryan Rahman <rahman.aryan07@gmail.com>
2023-09-01 18:40:32 +0600

    feat: used validators for character fields on job model

--- jobs/models.py
+++ jobs/models.py
@@ -1,16 +1,36 @@
 from django.db import models
-
+from django.core.validators import RegexValidator
 # Create your models here.
 
+
+alphanumericpunctuation_regex = '^[a-zA-Z0-9 _.,\-+=`~\|><@#$%^&*()!"\'/$]*$'
 class Job(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200,  validators=[
+        RegexValidator(
+            regex=alphanumericpunctuation_regex,
+            message='Only alphanumeric characters are allowed.',
+            code='invalid_characters'
+        )
+    ])
     description = models.TextField()
-    location = models.CharField(max_length=200)
-    company_name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200,  validators=[
+        RegexValidator(
+            regex=alphanumericpunctuation_regex,
+            message='Only alphanumeric characters are allowed.',
+            code='invalid_characters'
+        )
+    ])
+    company_name = models.CharField(max_length=200,  validators=[
+        RegexValidator(
+            regex=alphanumericpunctuation_regex,
+            message='Only alphanumeric characters are allowed.',
+            code='invalid_characters'
+        )
+    ])
     minimum_experience = models.IntegerField(default=0)
     application_url = models.URLField(blank=True)
     application_email = models.EmailField(blank=True)
-    date_added = models.DateTimeField("Date Published")
+    date_added = models.DateTimeField("Date Published", default='now')
 
     def __str__(self) -> str:
         return self.title + " " + self.company_name
