9104c158569338f1f88a45ed3b1415d0da49b645
Aryan Rahman <rahman.aryan07@gmail.com>
2023-09-13 11:17:23 +0600

    feat: implemented full text search 🪦🔍
    moira gesi eita implement korte jaiya

--- jobs/models.py
+++ jobs/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 from django.core.validators import RegexValidator
+from django.contrib.postgres.indexes import GinIndex
+from django.contrib.postgres.search import SearchVectorField, SearchVector
 from ckeditor.fields import RichTextField
 import nh3
 # Create your models here.
 
-
-alphanumericpunctuation_regex = '^[a-zA-Z0-9 _.,\-+=`~\|><@#$%^&*()!"\'/$]*$'
+alphanumeric_punctuation_regex = '^[a-zA-Z0-9 _.,\-+=`~\|><@#$%^&*()!"\'/$]*$'
 class Job(models.Model):
+    jobs = models.Manager()
+    class Meta:
+        # The search index pointing to our actual search field.
+        indexes = [GinIndex(fields=["search"])]
+    
     title = models.CharField(max_length=200,  validators=[
         RegexValidator(
-            regex=alphanumericpunctuation_regex,
+            regex=alphanumeric_punctuation_regex,
             message='Only alphanumeric characters are allowed.',
             code='invalid_characters'
         )
@@ -17,14 +23,14 @@ class Job(models.Model):
     description = RichTextField()
     location = models.CharField(max_length=200,  validators=[
         RegexValidator(
-            regex=alphanumericpunctuation_regex,
+            regex=alphanumeric_punctuation_regex,
             message='Only alphanumeric characters are allowed.',
             code='invalid_characters'
         )
     ])
     company_name = models.CharField(max_length=200,  validators=[
         RegexValidator(
-            regex=alphanumericpunctuation_regex,
+            regex=alphanumeric_punctuation_regex,
             message='Only alphanumeric characters are allowed.',
             code='invalid_characters'
         )
@@ -33,10 +39,17 @@ class Job(models.Model):
     application_url = models.URLField(blank=True)
     application_email = models.EmailField(blank=True)
     date_added = models.DateTimeField("Date Published", auto_now_add=True, blank=True)
+    search = SearchVectorField(null=True)
 
     def save(self, *args, **kwargs):
         self.description = nh3.clean(self.description)
         super().save(*args, **kwargs)
+        Job.jobs.filter(pk=self.id).update(search=(
+            SearchVector('title', weight='A') 
+            + SearchVector('description', weight='B')
+            + SearchVector('location', weight='C')
+            + SearchVector('company_name', weight='D')
+        ))
 
     def __str__(self) -> str:
         return self.title + " " + self.company_name
--- jobs/views.py
+++ jobs/views.py
@@ -26,5 +26,16 @@ class JobViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows jobs to be viewed or edited.
     """
-    queryset = Job.objects.all()
     serializer_class = JobSerializer
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the purchases for
+        the user as determined by the username portion of the URL.
+        """
+
+        if self.request.query_params.get('search'):
+            search = self.request.query_params.get('search')
+            return Job.jobs.filter(search=search)
+        else:
+            return Job.jobs.all()
